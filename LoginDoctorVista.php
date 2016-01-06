fc1299e24d9cb6a335ee4e30b1e84b4305de93b2
loresfca <loresfca.flores1@gmail.cpm>
2016-01-06 15:59:34 -0600

    Hello

---
+++ LoginDoctorVista.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="Frameworks/bootstrap-3.3.4-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="Frameworks/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
+         <link href="css/bootstrap-toggle.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <form id="login" method="post" action="MenuDoctor.php">
+            <label>Usuario</label>
+            <input type="text" value="" id="user">
+            <label>Contraseña</label>
+            <input type="password" id="passwd">
+            <input type="submit" value="Ingresar">
+        </form>
+    </body>
+</html>
---
+++ MenuDoctor.php
@@ -0,0 +1,30 @@
+
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Bienvenido</title>
+		<link rel="stylesheet" type="text/css" href="css/home.css" />
+	</head>
+	<body>
+		<div class="container">
+			<div class="main">
+				<ul class="cbp-ig-grid">
+					<li>
+						<a href="#">
+							<span class="icon plus"></span>
+							<h3 class="cbp-ig-title">Agregar Paciente</h3>
+						</a>
+					</li>
+					<li>
+						<a href="MenuDoctor.php">
+							<span class="icon doctor"></span>
+							<h3 class="cbp-ig-title">Consultar Paciente</h3>
+						</a>
+                    </li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
