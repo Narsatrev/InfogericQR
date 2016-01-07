5bee68261506eb00551221d4c57fd0fb7605eac8
David Manzanarez <david.manzanarez.v@gmail.com>
2016-01-07 08:30:49 -0600

    qr avance 2

--- js/base.js
+++ js/base.js
@@ -7,25 +7,74 @@ window.upload = function(data_forma) {
 var canvas=document.getElementById("canvas_qr");
 var ctx=canvas.getContext("2d");
 
-var imagen = new Image;
+var imagen = new Image;    
 imagen.src = URL.createObjectURL(data_forma);
-
+    
 imagen.onload = function() {
   var picWidth = this.width;
   var picHeight = this.height;
-var wdthHghtRatio = picHeight/picWidth;
-  console.log('wdthHghtRatio: ' + wdthHghtRatio);
+var ratio = picHeight/picWidth;
 
   if (Number(picWidth) < 5000) {
-    var newHeight = Math.round(Number(400) * wdthHghtRatio);
+    var newHeight = Math.round(Number(400) * ratio);
   } else {
     return false;
   };
     document.getElementById('canvas_qr').height = newHeight;
     ctx.drawImage(imagen,0,0, 400, newHeight);
 };
+    
 };
-var canvas_qr_decode=$("#canavas_qr");
-qrcode.decode(canvas_qr_decode,function(data){
-    console.log(data);
-});
+
+
+function setImageQR(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#img_qr_hidden').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
+function translateQR(){
+    qrcode.callback = showInfo;
+    var qr_img="";
+    qrcode.decode(qr_img);    
+}
+
+function showInfo(data) {
+    alert(data);
+}
+
+var file_name="";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   $('#form_send_qr').on('submit',(function(e) {
+        e.preventDefault();
+        var data_qr = new FormData(this);
+        $.ajax({
+            type:'POST',
+            url: $(this).attr('action'),
+            data:data_qr,
+            cache:false,
+            contentType: false,
+            processData: false,
+            success:function(data){
+                
+                console.log("success");
+                console.log(data);
+            },
+            error: function(data){
+                console.log("error");
+                console.log(data);
+            }
+        });
+    }));
+
+    $("#take-picture").on("change", function() {
+        console.log($(this).value);
+        $("#form_send_qr").submit();
+    });
+
