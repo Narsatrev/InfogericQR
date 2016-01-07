5ed294359e3f3fc8ebf9240aecd7d9d0e3c3a165
loresfca <loresfca.flores1@gmail.cpm>
2016-01-06 18:00:38 -0600

    Hola

--- registro_paciente.php
+++ registro_paciente.php
@@ -7,9 +7,107 @@
     </head>
     <body>
         <form id="registro" method="post" action="#">
-            <label>Nombre</label>
+            <div id="datos_generales" class="div-login" css="visibility:visible;">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Nombre</label>
+                    </div>
+                    <div class="col-sm-7">
+                        <input type="text" style="width:100%;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Edad</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-2">
+                        <label>Sexo</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Peso</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-2">
+                        <label>Talla</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                </div>
+            </div>
+            <div id="datos_medicos1" class="div-login" css="visibility:visible;">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Tipo de sangre</label>
+                    </div>
+                    <div class="col-sm-2" >
+                        <select style="width:100%; height:100%;">
+                            <option>A+</option>
+                            <option>A-</option>
+                            <option>B+</option>
+                            <option>B-</option>
+                            <option>O+</option>
+                            <option>O-</option>
+                            <option>AB+</option>
+                            <option>AB-</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-2">
+                        <label>Tabaquismo</label>                        
+                    </div>
+                    <div class="col-sm-1">
+                        Si <input type="radio" value="Si">                        
+                    </div>
+                    <div class="col-sm-1">
+                        No <input type="radio" value="No">                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Edad</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-2">
+                        <label>Sexo</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <label>Peso</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-2">
+                        <label>Talla</label>
+                    </div>
+                    <div class="col-sm-2">
+                        <input type="text" style="width:100%;">
+                    </div>
+                </div>
+            </div>
             <label>Datos de importancia médica</label>
-            <label>Edad</label>
+            
             <label>Alergias</label>
             <label>Tipos de sangre</label>
             <label>Auxiliares</label>
@@ -18,9 +116,6 @@
             <label>Teléfono de cuidador primario</label>
             <label>Telefono opcional</label>
             <label>Dirección del paciente</label>
-            <label>Sexo</label>
-            <label>Peso</label>
-            <label>Talla</label>
             <label>Tabaquismo</label>
             <label>Estado Civil</label>
             <label>Estado Nutricional</label>
