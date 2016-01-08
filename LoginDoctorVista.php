8dd3af38b6f4374ddd63fd49249a86ac85a11072
loresfca <loresfca.flores1@gmail.cpm>
2016-01-07 20:51:07 -0600

    sheller 3

--- LoginDoctorVista.php
+++ LoginDoctorVista.php
@@ -8,14 +8,32 @@
         <link rel="stylesheet" href="Frameworks/bootstrap-3.3.4-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="Frameworks/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
         <link href="css/bootstrap-toggle.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/login.css" />
     </head>
     <body>
+        <div id="barra_doc_up"></div>
         <form id="login" method="post" action="php/Login.php">
-            <label>Usuario</label>
-            <input type="text" value="" id="user">
-            <label>Contraseña</label>
-            <input type="password" id="passwd">
-            <input type="submit" value="Ingresar">            
+            <div class="row">
+                <div class="col-sm-3">
+                    <label>Usuario</label>
+                </div>
+                <div class="col-sm-6">
+                    <input type="text" value="" id="user" style="width:100%;">
+                </div>
+                
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <label>Contraseña</label>
+                </div>
+                <div class="col-sm-6">
+                   <input type="password" id="passwd" style="width:100%;">
+                </div>
+                
+            </div>
+            
+            <input type="submit" value="Ingresar" class="boton">  
         </form>
+        <div id="barra_doc_down"></div>
     </body>
 </html>
