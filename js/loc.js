f8167b2ffc8b120add183a728fa075a6bd454f7a
David Manzanarez <david.manzanarez.v@gmail.com>
2016-03-16 16:50:33 -0600

    direccion exacta agregado

--- js/loc.js
+++ js/loc.js
@@ -1,30 +1,50 @@
+            
+
             function direccion(lat, long) {
-                var dir="";
+                var dir="";    
                 var latitud_longitud = new google.maps.LatLng(lat, long);
                 var geocoder = geocoder = new google.maps.Geocoder();
                 geocoder.geocode({
                     'latLng': latitud_longitud 
                 }, function (results, status) {
                     if (status == google.maps.GeocoderStatus.OK) {
-                        console.log(results);
-                        if (results[1]) {
-                            dir= results[0].formatted_address;
+                        if (results[1]) {                                                        
+                            dir= results[0].formatted_address;                            
                         }else{
                             dir="La dirección no pudo recuperarse";
                         }
+                    }else{
+                        dir="La dirección no pudo recuperarse";
                     }
-                });
-                return dir;
+                    
+                    var nombre=$("#nombre_pac_mapa").val();                            
+                    var t= new Date();
+                    
+                    $.ajax({
+                        type:"POST",
+                        url:"mapa/alerta.php",
+                        data:{
+                            'lat':lat,
+                            'lng':long,
+                            'dir':dir,
+                            'nombre':nombre,
+                            'tiempo':t
+                        },
+                        cache:false,
+                        success(data){
+                            console.log("1");
+                        }
+                    });         
+                });  
             }
 
-            ////////DESDE AQUI IMPORTADO DE MAPA.JS
-              if(navigator.geolocation){
-                  
+              if(navigator.geolocation){                  
                 navigator.geolocation.getCurrentPosition(function(position) {
                   var pos={
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
                   };            
+                    
                     ////enviar a servidor datos de localizacion
                     $("#pos_lat").val(pos.lat);
                     $("#pos_long").val(pos.lng);
@@ -32,30 +52,12 @@
                     var lat=$("#pos_lat").val();
                     var lng=$("#pos_long").val();
                     
-                    var dir= direccion(lat,lng);
-                    var nombre=$("#nombre_pac_mapa").val();
-
-                    var t= new Date();
-                    $.ajax({
-                        type:"POST",
-                        url:"mapa/alerta.php",
-                        data:{
-                            'lat':lat,
-                            'lng':lng,
-                            'dir':dir,
-                            'nombre':nombre,
-                            'tiempo':t
-                             },
-                        cache:false,
-                        success(data){
-                            console.log("1");
-                        }
-                       });            
+                    //Buscar direccion + mandar mail
+                    direccion(lat,lng);
+                         
                 });
               }
-            ////////HASTA AQUI IMPORTADO DE MAPA.JS
-            
-            
+                        
             $("#button_QR").click(function(){
                 var qrcode = new QRCode(document.getElementById("qrcode_paciente"), {
                    width : 100,
